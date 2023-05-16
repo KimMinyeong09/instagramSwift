8b029e025f08321eb9df275906559583ec8a839b
KimMinyeong09 <minyeong9070@naver.com>
2023-05-16 14:31:29 +0900

    Initial Commit

---
+++ instagramSwift/instagramSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramSwiftApp.swift
+//  instagramSwift
+//
+//  Created by minyeong on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct instagramSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
