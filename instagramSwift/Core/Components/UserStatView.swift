a76b8fa5caf426c63c03ece3a5938d40bf0483c9
KimMinyeong09 <minyeong9070@naver.com>
2023-05-16 15:31:41 +0900

    [Feat] 프로필 뷰 생성

---
+++ instagramSwift/Core/Components/UserStatView.swift
@@ -0,0 +1,31 @@
+//
+//  UserStatView.swift
+//  instagramSwift
+//
+//  Created by minyeong on 2023/05/16.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    let value: Int
+    let title: String
+    
+    var body: some View {
+        VStack {
+            Text("\(value)")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            
+            Text("\(title)")
+                .font(.footnote)
+        }
+        .frame(width: 76)
+    }
+}
+
+struct UserStatView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserStatView(value: 12, title: "Posts")
+    }
+}
